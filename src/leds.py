8cd18fd7515b73781461707f825ce9e29b6ec065
𝒅𝒓𝒆𝒂𝒎𝒔 <mybeautifuladress@gmail.com>
2026-08-19 14:39:11 +0200

    added timeout code

--- src/leds.py
+++ src/leds.py
@@ -5,7 +5,7 @@ from machine import Pin, PWM
 # Posted by CrazyChucky, modified by community. See post 'Timeline' for change history
 # Retrieved 2026-08-14, License - CC BY-SA 4.0
 def map_range(x, in_min, in_max, out_min, out_max):
-  return (x - in_min) * (out_max - out_min) // (in_max - in_min) + out_min
+    return (x - in_min) * (out_max - out_min) // (in_max - in_min) + out_min
 
 
 class Led:
@@ -18,6 +18,7 @@ class Led:
 
     def off(self):
         self.led.value(0)
+
 
 class RGBLed:
     def __init__(self, pin_r, pin_g, pin_b):
@@ -39,13 +40,13 @@ class RGBLed:
         self.led_r.duty_u16(map_range(r, 0, 100, 0, 65535))
         self.led_g.duty_u16(map_range(g, 0, 100, 0, 65535))
         self.led_b.duty_u16(map_range(b, 0, 100, 0, 65535))
-    
+
     def off(self):
         self.led_r.duty_u16(0)
         self.led_g.duty_u16(0)
         self.led_b.duty_u16(0)
 
-    def set_mode(self, mode):   
+    def set_mode(self, mode):
         self.off()
         self.current_mode = mode
         self.new_mode = True
@@ -53,37 +54,70 @@ class RGBLed:
 
     def update(self, current_time):
         if self.current_mode == "menu_lights":
-
             if self.new_mode:
                 self.new_mode = False
                 # mode setup
 
                 step = 20
-                phase1 = [
-                    [val, val, val]
-                    for val in range(step + 1)
-                ]
-                phase2 = [
-                    [val, val, step]
-                    for val in range(step - 1, -1, -1)
-                ]
-                self.phase = phase1 + phase2
+                phase1 = [[val, val, val] for val in range(step + 1)]
+                phase2 = [[val, val, step] for val in range(step - 1, -1, -1)]
+                phase3 = [[0, 0, val] for val in range(step - 1, -1, -1)]
+                self.phase = phase1 + phase2 + phase3
 
             if current_time - self.last_millis >= 30:
-                self.set_color(self.phase[self.counter][0], self.phase[self.counter][1], self.phase[self.counter][2]) 
+                self.set_color(
+                    self.phase[self.counter][0],
+                    self.phase[self.counter][1],
+                    self.phase[self.counter][2],
+                )
                 self.counter += 1
                 if self.counter >= len(self.phase):
                     self.counter = 0
 
-                self.last_millis = current_time
-                 
-        elif self.current_mode == "menu_sockets": 
+                    self.last_millis = current_time
+
+        elif self.current_mode == "menu_sockets":
             if self.new_mode:
                 self.new_mode = False
                 # mode setup
 
-        elif self.current_mode == "menu_radio": 
+                step = 20
+                phase1 = [[val, val, val] for val in range(step + 1)]
+                phase2 = [[val, step, val] for val in range(step - 1, -1, -1)]
+                phase3 = [[0, val, 0] for val in range(step - 1, -1, -1)]
+                self.phase = phase1 + phase2 + phase3
 
+            if current_time - self.last_millis >= 30:
+                self.set_color(
+                    self.phase[self.counter][0],
+                    self.phase[self.counter][1],
+                    self.phase[self.counter][2],
+                )
+                self.counter += 1
+                if self.counter >= len(self.phase):
+                    self.counter = 0
+
+                    self.last_millis = current_time
+
+        elif self.current_mode == "menu_radio":
             if self.new_mode:
                 self.new_mode = False
                 # mode setup
+                step = 20
+
+                phase1 = [[val, val, val] for val in range(step + 1)]
+                phase2 = [[step, val, val] for val in range(step - 1, -1, -1)]
+                phase3 = [[val, 0, 0] for val in range(step - 1, -1, -1)]
+                self.phase = phase1 + phase2 + phase3
+
+            if current_time - self.last_millis >= 30:
+                self.set_color(
+                    self.phase[self.counter][0],
+                    self.phase[self.counter][1],
+                    self.phase[self.counter][2],
+                )
+                self.counter += 1
+                if self.counter >= len(self.phase):
+                    self.counter = 0
+
+                    self.last_millis = current_time
--- src/main.py
+++ src/main.py
@@ -1,6 +1,11 @@
-import apps, inputs, leds, remote_hack, xiao, buzzer, home_assistant
 import time
 
+import buzzer
+import home_assistant
+import inputs
+import leds
+import remote_hack
+import xiao
 
 epaper = xiao.Display()
 ha = home_assistant.HomeAssistant(epaper.uart)
@@ -20,7 +25,7 @@ buzz = buzzer.Buzzer(5)
 led_rgb = leds.RGBLed(11, 13, 12)
 
 pot = inputs.Pot(28)
-
+last_pot_value = pot.read()
 
 
 def clean():
@@ -29,10 +34,11 @@ def clean():
     led_c.off()
     led_rgb.off()
 
+
 def test():
-    led_rgb.set_color(100,0, 0)
+    led_rgb.set_color(100, 0, 0)
     time.sleep(1)
-    led_rgb.set_color(0,100, 0)
+    led_rgb.set_color(0, 100, 0)
     time.sleep(1)
     led_rgb.set_color(0, 0, 100)
     time.sleep(1)
@@ -50,8 +56,6 @@ def test():
     led_b.off()
     led_c.off()
 
-    
-
     i = 0
     while i < 200:
         print("pot: " + str(pot.read()))
@@ -67,11 +71,11 @@ def test():
         i += 1
         time.sleep(0.1)
 
-    
     remote.press("c")
     epaper.show_image("logo", img_r=None)
 
     buzz.bip()
+
 
 # test()
 
@@ -80,49 +84,51 @@ def test():
 #     while True:
 #         time.sleep(0.01)
 #         now = time.ticks_ms()
-        
 
-        # for i in range(100):
 
-        #     led_rgb.set_color(0, i, int(i / 2))
-        #     time.sleep_ms(2)
-        # for i in range(100):
-        #     led_rgb.set_color(0, abs(i - 100) , int(abs(i - 100) / 2))
-        #     time.sleep_ms(7)
+# for i in range(100):
 
-        # step = 20
+#     led_rgb.set_color(0, i, int(i / 2))
+#     time.sleep_ms(2)
+# for i in range(100):
+#     led_rgb.set_color(0, abs(i - 100) , int(abs(i - 100) / 2))
+#     time.sleep_ms(7)
 
-        # for i in range(step):
-        #     led_rgb.set_color(i, 0, abs(i - step))
-        #     time.sleep_ms(20)
-        # for i in range(step):
-        #     led_rgb.set_color(abs(i - step), i , 0)
-        #     time.sleep_ms(20) 
-        # for i in range(step):
-        #     led_rgb.set_color(0, abs(i - step), i)
-        #     time.sleep_ms(20) 
+# step = 20
 
-        # for i in range(step):
-        #     led_rgb.set_color(i, i, i)
-        #     time.sleep_ms(20)
-        # for i in range(step):
-        #     led_rgb.set_color(abs(i - step), abs(i - step), step)
-        #     time.sleep_ms(10)   
-        # for i in range(step):
-        #     led_rgb.set_color(0, 0, abs(i - step))
-        #     time.sleep_ms(20)
+# for i in range(step):
+#     led_rgb.set_color(i, 0, abs(i - step))
+#     time.sleep_ms(20)
+# for i in range(step):
+#     led_rgb.set_color(abs(i - step), i , 0)
+#     time.sleep_ms(20)
+# for i in range(step):
+#     led_rgb.set_color(0, abs(i - step), i)
+#     time.sleep_ms(20)
 
-        # for i in range(33):
-        #     led_rgb.set_color(abs(i - 33), abs(i - 33), abs(i - 33))
-        #     time.sleep_ms(10)
-                        
+# for i in range(step):
+#     led_rgb.set_color(i, i, i)
+#     time.sleep_ms(20)
+# for i in range(step):
+#     led_rgb.set_color(abs(i - step), abs(i - step), step)
+#     time.sleep_ms(10)
+# for i in range(step):
+#     led_rgb.set_color(0, 0, abs(i - step))
+#     time.sleep_ms(20)
+
+# for i in range(33):
+#     led_rgb.set_color(abs(i - 33), abs(i - 33), abs(i - 33))
+#     time.sleep_ms(10)
+
 
 # test2()
 
 current_global_state = "MENU"
 last_menu_state = None
 current_menu_state = "LIGHTS"
-
+is_active = False
+displayed_menu_state = None
+last_activity_time = time.ticks_ms()
 
 
 try:
@@ -142,13 +148,20 @@ try:
         current_btn_bck_state = button_bck.is_pressed(now)
         current_btn_frw_state = button_frw.is_pressed(now)
         current_btn_hme_state = button_hme.is_pressed(now)
-        current_pot_value = pot.read()
+        current_pot_value = pot.read()  
+
+        is_active = False
+        if current_btn_bck_state == "RELEASED" or current_btn_frw_state == "RELEASED" or current_btn_hme_state == "RELEASED":
+            is_active = True
+        if abs(current_pot_value - last_pot_value) >= 2:
+            is_active = True
+
+        if is_active:
+            last_activity_time = now
 
         # logic
         if current_global_state == "MENU":
-
             if current_btn_bck_state == "RELEASED":
-
                 if current_menu_state == "LIGHTS":
                     current_menu_state = "RADIO"
                 elif current_menu_state == "SOCKETS":
@@ -157,8 +170,7 @@ try:
                     current_menu_state = "SOCKETS"
 
             elif current_btn_frw_state == "RELEASED":
-
-                if current_menu_state ==  "LIGHTS":
+                if current_menu_state == "LIGHTS":
                     current_menu_state = "SOCKETS"
                 elif current_menu_state == "SOCKETS":
                     current_menu_state = "RADIO"
@@ -166,25 +178,32 @@ try:
                     current_menu_state = "LIGHTS"
 
             elif current_btn_hme_state == "RELEASED":
-
                 current_global_state = current_menu_state
 
         # display
         if current_global_state == "MENU":
             if current_menu_state != last_menu_state:
-                # print("update")
-                if current_menu_state =="LIGHTS":
+                if current_menu_state == "LIGHTS":
                     led_rgb.set_mode("menu_lights")
-                    epaper.show_image("menu_light_b", "menu_light_r")
-                elif current_menu_state =="SOCKETS":
+                elif current_menu_state == "SOCKETS":
                     led_rgb.set_mode("menu_sockets")
-                    epaper.show_image("menu_socket_b", "menu_socket_r")
                 elif current_menu_state == "RADIO":
                     led_rgb.set_mode("menu_radio")
-                    epaper.show_image("menu_radio_b", "menu_radio_r")
+
+            if isinstance(last_activity_time, int) and now - last_activity_time >= 2000:
+                if displayed_menu_state != current_menu_state:
+           
+                    if current_menu_state == "LIGHTS":
+                        epaper.show_image("menu_light_b", "menu_light_r")
+                    elif current_menu_state == "SOCKETS":
+                        epaper.show_image("menu_socket_b", "menu_socket_r")
+                    elif current_menu_state == "RADIO":
+                        epaper.show_image("menu_radio_b", "menu_radio_r")
+
+                    displayed_menu_state = current_menu_state
 
         last_menu_state = current_menu_state
-
+        last_pot_value = current_pot_value
 
 except KeyboardInterrupt:
     print("[main.py] IMPORTANT: asked for stop the main program")
